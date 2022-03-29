465df00476a43a25b32c3cb18fc0c74311f40de2
Juan Pablo Restrepo <jrestrepo@azimutenergia.co>
2022-03-29 16:49:44 -0500

    Afreguee suscripcion, cambie la cabecera maestra y puse todo azul

--- micodigo.py
+++ micodigo.py
@@ -9,4 +9,6 @@ def llenado_de_huecos():
 print('agregue u parrafo de lo profundo de mi corazón')
 print('cabecera')
 
-print('modificaciones a la cabecera')
+print('este es tu blog maestro')
+
+print('suscribete y dale a likes')
